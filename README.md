c6006bfaf564b52b9a18b8e2bf3a719650633725
Delziron Lima <delziron.braz@hotmail.com>
2024-07-07 20:22:37 -0300

    setup project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Teste-Técnico-Front-End-DelzironBraz
+
+O teste técnico consiste no desenvolvimento de uma aplicação web de comércio digital simples onde os usuários possam visualizar, adicionar e remover produtos de um carrinho de compras. O projeto foi desenvolvido usando React + Typescript, pois é o framework que possuo maior domínio.
+
+## Como executar ?
+
+```bash
+npm i
+```
+
+```bash
+npm run dev
+```
---
+++ src/services/ProductService.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export const productService = {
+    getAllProducts: async () => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_API_URL}/products`);
+
+            if (response.status !== 200) {
+                throw new Error('Error fetching products');
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            throw error;
+        }
+    },
+    getProductById: async (id: number) => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_API_URL}/products/${id}`);
+
+            if (response.status !== 200) {
+                throw new Error('Error fetching products');
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            throw error;
+        }
+    },
+    search: async (searchProduct: string) => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_BASE_API_URL}/products/search?q${searchProduct}`);
+
+            if (response.status !== 200) {
+                throw new Error('Error fetching products');
+            }
+
+            return response.data;
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            throw error;
+        }
+    },
+};
