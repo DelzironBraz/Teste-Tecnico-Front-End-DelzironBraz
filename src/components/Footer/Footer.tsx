8315ca600a1499581424b77423616d5f1fe22d9c
Delziron Lima <delziron.braz@hotmail.com>
2024-07-10 01:37:54 -0300

    feat: create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <div>
+                    <a href="/" className='logo'>E-commerce</a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
