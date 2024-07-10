7eb0c4132217f4e1a463c5c4ac4bd8647bd6d75f
Delziron Lima <delziron.braz@hotmail.com>
2024-07-10 01:40:05 -0300

    feat: create cart page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './pages/Cart/Cart';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/carrinho' element={<Cart />} />
       </Routes >
+      <Footer />
     </>
   )
 }
---
+++ src/pages/Cart/Cart.tsx
@@ -0,0 +1,12 @@
+import './Cart.scss';
+import CartSection from '../../components/Cart/CartSection/CartSection';
+
+const Cart = () => {
+    return (
+        <main className='cart'>
+            <CartSection />
+        </main>
+    );
+};
+
+export default Cart;
